2f8af01f7269e4aba360fa79dc371a0a896c9a0e
qminhminh <hqminh050503@gmail.com>
2024-09-14 10:17:57 +0700

    ok

--- lib/bai_tap_5.dart
+++ lib/bai_tap_5.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_element
+
 import 'package:flutter/material.dart';
 
 class BaiTap05 extends StatefulWidget {
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
 
 import 'package:bai_tap_van_dung_cty/bai_tap_1.dart';
 import 'package:bai_tap_van_dung_cty/bai_tap_2.dart';
@@ -28,46 +28,29 @@ class MyApp extends StatelessWidget {
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final List<Map<String, dynamic>> exercises = [
+      {'label': 'Bài tập 1', 'destination': BaiTap01()},
+      {'label': 'Bài tập 2', 'destination': BaiTap02()},
+      {'label': 'Bài tập 3', 'destination': BaiTap03()},
+      {'label': 'Bài tập 4', 'destination': BaiTap04()},
+      {'label': 'Bài tập 5', 'destination': BaiTap05()},
+      {'label': 'Bài tập 6', 'destination': BaiTap06()},
+      {'label': 'Bài tập 7', 'destination': BaiTap07()},
+      {'label': 'Bài tập 8', 'destination': BaiTap08()},
+    ];
+
     return Scaffold(
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: SingleChildScrollView(
             child: Column(
-              children: [
-                ExerciseButton(
-                  label: 'Bài tập 1',
-                  destination: BaiTap01(),
-                ),
-                ExerciseButton(
-                  label: 'Bài tập 2',
-                  destination: BaiTap02(),
-                ),
-                ExerciseButton(
-                  label: 'Bài tập 3',
-                  destination: BaiTap03(),
-                ),
-                ExerciseButton(
-                  label: 'Bài tập 4',
-                  destination: BaiTap04(),
-                ),
-                ExerciseButton(
-                  label: 'Bài tập 5',
-                  destination: BaiTap05(),
-                ),
-                ExerciseButton(
-                  label: 'Bài tập 6',
-                  destination: BaiTap06(),
-                ),
-                ExerciseButton(
-                  label: 'Bài tập 7',
-                  destination: BaiTap07(),
-                ),
-                ExerciseButton(
-                  label: 'Bài tập 8',
-                  destination: BaiTap08(),
-                ),
-              ],
+              children: exercises.map((exercise) {
+                return ExerciseButton(
+                  label: exercise['label'] as String,
+                  destination: exercise['destination'] as Widget,
+                );
+              }).toList(),
             ),
           ),
         ),
